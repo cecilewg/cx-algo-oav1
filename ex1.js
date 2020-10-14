cc57b87988a3dfc8d0f63cc3f14b5b517f85ec2b
Cecile WANG cecile_wang_95@hotmail.fr <cecile_wang_95@hotmail.fr>
2020-10-14 09:40:19 +0200

    first exercice resolved

---
+++ ex1.js
@@ -0,0 +1,27 @@
+// 01 HowManyCamelCase
+// Create a function called howManyCamelCase that will take one string input str.
+// We are given a sequence of words that are sandwiched together with no space.
+// With the exception of the first word, in all of the subsequent words, the first letter of each word is capitalized.
+
+// The goal of the function is to output the number of words that are in the camelcase string
+
+function isUpperCase (character) {
+  if (character.match(/^[A-Z]/)) {
+    return true
+  } else {
+    return false
+  }
+}
+
+function howManyCamelCase (str) {
+  var words = 1
+
+  for (i = 0; i < str.length; i++) {
+    if(isUpperCase(str.charAt(i))) {
+      words += 1
+    }
+  }
+  return words
+}
+
+console.log(howManyCamelCase('appleOrangePearGrapes'))
